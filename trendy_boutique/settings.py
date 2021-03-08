b07582d427d1b5f6953b8c9af97453f747366106
Tattered75 <YMoyo@cabotfinancial.ie>
2021-03-08 17:56:05 +0000

    making amendmentmenst to deploy heroku

--- trendy_boutique/settings.py
+++ trendy_boutique/settings.py
@@ -120,15 +120,16 @@ WSGI_APPLICATION = 'trendy_boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse('postgres://azbtiltqgvicbr:2b03e49cf72002263f738b77f166abed8ee240d0ad9e4b4eb61ebe620eaa34e2@ec2-54-228-9-90.eu-west-1.compute.amazonaws.com:5432/dcl9hfq5queebp)')
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 
 
